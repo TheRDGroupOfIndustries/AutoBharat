df89605de377dcde6dbed64621ad2fe2cf22ece0
sujitkumar-13 <sujitkumar19013@gmail.com>
2026-01-17 16:08:29 +0530

    feat: Add `replace` prop to blog post back-to-blogs link.

--- components/BlogPostContent.tsx
+++ components/BlogPostContent.tsx
@@ -47,6 +47,7 @@ export default function BlogPostContent({ post, imageUrl }: BlogPostContentProps
             >
                 <Link
                     href="/#blogs"
+                    replace
                     className="inline-flex items-center gap-2 px-4 py-2 bg-black/40 backdrop-blur-md rounded-full border border-white/10 hover:bg-white/10 transition-all group"
                 >
                     <ArrowLeft className="w-5 h-5 group-hover:-translate-x-1 transition-transform" />
